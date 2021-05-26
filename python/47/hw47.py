8b16517be27fdd751f36757c964bb33d847cb153
zrobinson613 <zrobinson613@gmail.com>
2021-05-26 18:06:44 -0400

    hw

--- python/47/hw47.py
+++ python/47/hw47.py
@@ -32,6 +32,6 @@ for i in range(1, 101):
         print('Fizz ', end='')
     if i % 5 == 0:
         print('Buzz ', end='')
-    if i % 3 != 0 and i % 5 != 0:
+    elif i % 3 != 0 and i % 5 != 0:
         print(i, end='')
     print('')
