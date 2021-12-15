20fed164a447b82d4ba07f3bb4d971057ad572cd
zrobinson613 <zrobinson613@gmail.com>
2021-12-14 22:04:03 -0500

    hw

--- js/84/hw.js
+++ js/84/hw.js
@@ -4,9 +4,7 @@
     let theDiv = $('body').append($("<div>"));
 
     theDiv.append($('<button/>').text("button").click(()=>{
-        $('body').css("background-color", "orange");
+        $('body').css("background-color", `#${Math.floor(Math.random()*16777215).toString(16)}`);
     }));
-
-
 
 })();
